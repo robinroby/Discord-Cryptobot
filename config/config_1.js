7a1d32ac20eaaf33287b98d7e84fbde2a75696de
robinroby <vietlhp@yahoo.de>
2021-03-16 00:54:41 +0100

    Add files via upload

---
+++ config/config_1.js
@@ -0,0 +1,19 @@
+module.exports.options = {
+    token: 'YOUR DISCORD BOT TOKEN',
+    apiKey: '',    
+    api: 'https://min-api.cryptocompare.com/data/pricemultifull',
+    pairs: [
+      'BTC',
+      'ETH'
+      
+                
+    ],
+    exchange: 'Coinbase',
+    currencies: [      
+      'USD',
+      'EUR'
+    ],
+    pollRate: 15,
+};
+
+//Bot activity allows only 4 pairs
